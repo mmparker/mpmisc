ddad06f7e52040de099a147aac2d47730a07a034
Matt Parker <mattmparker@gmail.com>
2013-06-18 10:56:42 -0600

    Add obsolescence warning to get_prior_monday.

--- get_prior_monday.r
+++ get_prior_monday.r
@@ -4,6 +4,9 @@
 
 get_prior_monday <- function(Date, weeks.back = 1) {
 
+    # Warn users to switch to get_week_start
+    warning("get_prior_monday() is obsolete - please switch to the function get_week_start()")
+
     # Get the names of the next seven days
     next.seven <- seq(from = Date, length.out = 7, by = "day")
 
