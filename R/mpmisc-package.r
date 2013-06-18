d77a3094cd8c75544177ad18092de58027f06318
Matt Parker <mattmparker@gmail.com>
2013-06-18 13:24:15 -0600

    Add package documentation and specify dependencies.

---
+++ R/mpmisc-package.r
@@ -0,0 +1,12 @@
+#' mpmisc: a collection of miscellaneous functions for report-making.
+#' 
+#' This package provides a collection of miscellaneous functions for 
+#' report-making around the TB Clinic.
+#' 
+#' @name mpmisc-package
+#' @aliases mpmisc
+#' @docType package
+#' @author Matt Parker \email{mattmparker@@gmail.com}
+#' @keywords package misc
+#' @import xtable RODBC
+NULL
